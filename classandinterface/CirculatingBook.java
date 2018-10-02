4b045db521b2bcaf371168104b4ffcc70ac43967
citassy <43353001+citassy@users.noreply.github.com>
2018-10-02 11:12:59 -0500

    commented

--- classandinterface/CirculatingBook.java
+++ classandinterface/CirculatingBook.java
@@ -1,36 +1,40 @@
 package classandinterface;
 
 public class CirculatingBook extends LibraryBook implements Comparable<LibraryBook>{
+	
+	//adds 2 fields to Circulating books
 	protected String currentHolder;
 	protected String dueDate;
 
+	//defines a circulating book by 4 parameters
 	public CirculatingBook(String author, String name, String code, String num) {
 		super(author, name, code, num);
 		currentHolder = null;
 		dueDate = null;
 	}
 	
+	//returns the name of the patron who is having the book
 	public String getCurrentHolder() {
 		return currentHolder;
 	}
 	
+	//returns the date by the book needs to be returned
 	public String getDueDate() {
 		return dueDate;
 	}
 
+	//assigns a current holder to the book
 	public void setCurrentHolder(String curr) {
 		currentHolder = curr;
 	}
 	
+	//assigns a due date to the book by which it needs to be returned
 	public void setDueDate(String due) {
 		dueDate = due;
 	}
 
-	public int compareTo(ReferenceBook arg0) {
-		return 0;
-	}
-	
-	@Override
+	//return that the book is not checked out if it is not
+	//returns the holder's name and the due date if it is checked out
 	public String circulationStatus() {
 		if (currentHolder == null) {
 			return "currently not checked out";
@@ -40,18 +44,20 @@ public class CirculatingBook extends LibraryBook implements Comparable<LibraryBo
 		}
 	}
 
-	@Override
+	//sets the name of the currentHolder
+	//sets the dueDate by which the book needs to be returned
 	public void checkout(String patron, String due) {
 		currentHolder = patron;
 		dueDate = due;
 	}
 
-	@Override
+	//resets all to null as no one checked the book out
 	public void returned() {
 		currentHolder = null;
 		dueDate = null;
 	}
 	
+	//prints out the information of a circulation book
 	public String toString() {
 		return super.toString() + 
 				"currentHolder: " + currentHolder 
@@ -59,3 +65,4 @@ public class CirculatingBook extends LibraryBook implements Comparable<LibraryBo
 	}
 
 }
+
--- classandinterface/ReferenceBook.java
+++ classandinterface/ReferenceBook.java
@@ -1,40 +1,46 @@
 package classandinterface;
 
 public class ReferenceBook extends LibraryBook implements Comparable<LibraryBook>{
+	
+	//adds a new field for ReferenceBooks
 	protected String collection;
 
-	
+	//constructs a ReferenceBook that has 5 parameters
 	public ReferenceBook(String author, String name, String code, String num, String coll) {
 		super(author, name, code, num);
 		collection = coll; 
 	}
 	
+	//returns the collection type of the book
 	public String getCollection() {
 		return collection;
 	}
 
+	//assigns the book to a collection
 	public void setCollection(String coll) {
 		collection = coll;
 	}
-
-	@Override
+	
+	//Reference books cannot be circulated
 	public String circulationStatus() {
 		return "non-circulating reference book";
 	}
 
-	@Override
+	//Reference books cannot be checked out
 	public void checkout(String patron, String due) {
 		System.out.println(patron + "cannot check out a reference book until");		
 	}
 
-	@Override
+	//Reference books cannot be returned
 	public void returned() {
 		System.out.println("reference book could not have been checked out -- return impossible");
 		
 	}
 	
+	//prints out the information of the reference book
 	public String toString() {
 		return super.toString() + 
 				"Collection: " + collection;
 	}
 }
+
